1580569f42819500bf39588d1595579d37e8fde4
aswerty123 <chirpy_melody@hotmail.com>
2023-09-03 19:00:04 +0800

    First Commit to Test AWS Deployment with MS

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { Header } from './components/Header';
+import { Login } from './pages/Login';
+import { PrivateRoutes } from './utils/PrivateRoutes';
+import { Home } from './pages/Home';
+import { SearchAllProduct } from './pages/SearchAllProduct';
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path='/search' element={<SearchAllProduct/>} />
+      {/* <Route path="/" element={<Home/>}/> */}
+        <Route path='/login' element={<Login/>}/>
+        <Route element={<PrivateRoutes />}>
+        <Route path="/" element={<Home/>}/>
+          </Route>
+      </Routes>
+    </Router>
+    </>
+  );
+}
+
+export default App;
